90cc1e0570ff44d68f90edd8780fefb95920bf71
Muhammad Nouman Butt <76642732+baelessboi@users.noreply.github.com>
2021-05-24 08:54:58 +0600

    final commit gui

---
+++ DLPs/src/dlps/DLPs.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dlps;
+
+/**
+ *
+ * @author m_nou
+ */
+public class DLPs {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+//        LoginFrame g = new LoginFrame();
+//        g.setVisible(true);
+generation.startProgram();
+    }
+    
+}
